eaa1b72e7dcd4c455a6a8c6312b5d431ccaca6bf
A <a@gmail.como>
2024-03-25 17:03:37 +0200

    Init commit

---
+++ CharityCab/CharityCab/Application/CharityCabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharityCabApp.swift
+//  CharityCab
+//
+//  Created by artem on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CharityCabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CharityCab/CharityCab/Presentation/Stories/LoadingView.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingView.swift
+//  CharityCab
+//
+//  Created by artem on 14.03.2024.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
